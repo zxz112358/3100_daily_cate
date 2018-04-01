3931528f76f2f320e527dea891c3d9f676f582c7
liumuzi <liumuzi1215@gmail.com>
2018-04-02 01:10:13 +0800

    all article

--- routes/exhibitionSec/exhibition.js
+++ routes/exhibitionSec/exhibition.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var test = require('../test');
+var fs=require("fs");
 
 /* GET exhibition page. */
 router.get('/', function(req, res, next) {
@@ -16,6 +17,14 @@ router.get('/', function(req, res, next) {
             });
         }
         else{
+            var textpath = [];
+            var text = [];
+            for(var i=0;i<result1;i++){
+                textpath.push(result2[i].parastart+result2[i].parano-1);
+                textpath[i]='./routes/exhibitionSec/texts/'+textpath[i];
+                var data=fs.readFileSync(textpath[i]);
+                text.push(data.toString());
+            }
             console.log(result2);
             console.log(result1);
             console.log(result2[0].articlename);
@@ -24,7 +33,8 @@ router.get('/', function(req, res, next) {
                 name:'Daily Cate',
                 user: req.user,
                 allArticle: result2,
-                articleNum: result2.length
+                articleNum: result2.length,
+                text: text
             });
         }
     });
