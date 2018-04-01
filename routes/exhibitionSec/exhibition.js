a2218be998b577d784c925d915650865e82ad6bd
liumuzi <liumuzi1215@gmail.com>
2018-04-02 00:24:27 +0800

    fsdgd

--- routes/exhibitionSec/exhibition.js
+++ routes/exhibitionSec/exhibition.js
@@ -1,13 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+var test = require('../test');
+
 /* GET exhibition page. */
 router.get('/', function(req, res, next) {
-  res.render('exhibitionSec/exhibition', {
-    title: 'Exhibition',
-    name:'Daily Cate',
-    user: req.user
-  });
+    test.select_all_article(function(result1,result2){
+        if(result1===0){
+            console.log("no results found.");
+            res.render('exhibitionSec/exhibition', {
+                title: 'Exhibition',
+                name:'Daily Cate',
+                user: req.user,
+                allArticle: undefined
+            });
+        }
+        else{
+            console.log(result2);
+            console.log(result1);
+            console.log(result2[0].articlename);
+            res.render('exhibitionSec/exhibition', {
+                title: 'Exhibition',
+                name:'Daily Cate',
+                user: req.user,
+                allArticle: result2,
+                articleNum: result2.length
+            });
+        }
+    });
 });
 
 module.exports = router;
