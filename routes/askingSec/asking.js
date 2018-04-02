27529ed3364fe4b4d6051190652f2bbd9ce942f1
liumuzi <liumuzi1215@gmail.com>
2018-04-03 00:17:39 +0800

    help section article clickable

--- routes/askingSec/asking.js
+++ routes/askingSec/asking.js
@@ -72,5 +72,26 @@ router.get('/', function(req, res, next) {
     });
 });
 
+router.post('/', function (req, res, next) {
+    var result = String(req.body.result).match(/[^\d]+|\d+/g);//result[0] is tag, result[1] is index
+    console.log('result: ', result);
+
+    new Promise(
+        function (resolve, reject) {
+            if (result[0] === 'all'){
+                test.select_all_article('help', function(num, articleList){
+                    resolve(encodeURIComponent(articleList[result[1]].articleID));
+                })
+            } else {
+                test.select_article_list(result[0], 'help', function(num, articleList){
+                    resolve(encodeURIComponent(articleList[result[1]].articleID));
+                })
+            }
+        }
+    ).then(function (value) {
+        res.redirect('./helpPost?articleId=' + value);
+    });
+});
+
 module.exports = router;
 console.log("this is the Asking for help page!");
