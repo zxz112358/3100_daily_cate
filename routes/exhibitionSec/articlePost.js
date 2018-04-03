dfa180ceed6c9ca6a660b714e68d6bfc5edc9b3c
970227natalie <abcdefg19976@gmail.com>
2018-04-03 21:47:12 +0800

    dsfgafgerg

--- routes/exhibitionSec/articlePost.js
+++ routes/exhibitionSec/articlePost.js
@@ -27,12 +27,12 @@ router.post('/', function(req, res, next) {
     var comment = req.body.comment;
     var like_operation = req.body.operation;
 
-    var authorname = String(req.body.author);
+    var authorname = req.body.author;
 
     console.log('comment: ', comment, 'like_ope: ', like_operation, 'au:', authorname);
 
     if (comment || like_operation || authorname) {
-        if (comment !== undefined) {
+        if (comment) {
             test.count_comment(function (commentNum) {
                 test.insert_comment(commentNum + 1, req.user.username, req.body.comment, req.query.articleId);
             });
@@ -46,7 +46,7 @@ router.post('/', function(req, res, next) {
             test.unlike(req.user.username, req.query.articleId);
             res.redirect('back');
         }
-        if (authorname !== undefined){
+        if (authorname){
             var username = encodeURIComponent(authorname);
             res.redirect('../personalSec/userpostpage?username=' + username);
         }
