6f999c2c8b3a3d2b4643f3d3730f8e58a119f079
liumuzi <liumuzi1215@gmail.com>
2018-04-03 10:44:56 +0800

    ingredient post articles clickable

--- routes/ingredientSec/ingredientPost.js
+++ routes/ingredientSec/ingredientPost.js
@@ -5,11 +5,8 @@ var test = require('../test');
 
 /* GET exhibition page. */
 router.get('/', function(req, res, next) {
-    //console.log("!!!!",decodeURIComponent(req.query.articleId));
     test.select_article(decodeURIComponent(req.query.articleId), function (article) {
         test.select_article_comment(decodeURIComponent(req.query.articleId),function(result1,result2){
-            //console.log(result1);//the no. of comments in that article;
-            //console.log(result2);//all comments -> result2[i].content
             res.render('ingredientSec/ingredientPost', {
                 title: 'ingredientPost',
                 name: 'Daily Cate',
@@ -17,8 +14,12 @@ router.get('/', function(req, res, next) {
                 article: article,
                 comment:result2
             });
-
+            console.log(decodeURIComponent(req.query.articleId));
+            console.log('comments: ', result2);
         });
-        //console.log(article);
     });
 });
+
+module.exports = router;
+
+
